dbe6caf94406fe5049a7d2a184c081e9b1447b9a
BorniaPedro <borniaph@gmail.com>
2025-04-01 14:20:18 -0300

    Adiciona atividade prática 1

---
+++ pratica1.py
@@ -0,0 +1,31 @@
+# Definindo transições
+transicoes = {
+    ('q00', 'a'): 'q10',
+    ('q00', 'b'): 'q01',
+    ('q10', 'a'): 'q00',
+    ('q10', 'b'): 'q11',
+    ('q01', 'a'): 'q11',
+    ('q01', 'b'): 'q02',
+    ('q11', 'a'): 'q01',
+    ('q11', 'b'): 'q12',
+    ('q02', 'a'): 'q12',
+    ('q02', 'b'): 'q00',
+    ('q12', 'a'): 'q02',
+    ('q12', 'b'): 'q10'
+}
+
+estado_inicial = 'q00'
+estados_finais = {'q10'}
+
+def afd(cadeia):
+    estado_atual = estado_inicial
+    for simbolo in cadeia:
+        estado_atual = transicoes[(estado_atual, simbolo)]
+    return estado_atual in estados_finais
+
+# Testes
+print (afd('abbb'))  # True
+print (afd('ab'))  # False
+print (afd('ababab'))  # True
+print (afd('bbb'))  # False
+print (afd('abbbabbba'))  # True
